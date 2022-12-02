7a92a59a983b811ce3650c8bf47547120cdea2de
Donovan Daniels <hewwo@yiff.rocks>
2022-12-02 10:43:11 -0600

    proper esm export

--- esm.mjs
+++ esm.mjs
@@ -1,2 +1,6 @@
 const Yiffy = (await import("./dist/lib/index.js")).default.default;
+export * from "./dist/lib/util/Constants.js";
+const APIError = (await import("./dist/lib/util/APIError.js")).default.default;
+
+export { APIError };
 export default Yiffy;
